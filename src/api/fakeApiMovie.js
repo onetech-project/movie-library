09fbff526750ce87b10f942a306b0ac6b8f2e95b
faris <putra.faris295@gmail.com>
2021-09-01 23:28:08 +0700

    - add movie api - add movie image - add movie component - add moment

--- src/api/fakeApiMovie.js
+++ src/api/fakeApiMovie.js
@@ -1,14 +1,60 @@
-//example api request: replace with your API request here in folder API
+import { breakingbad, kingdom, moneyheist, peakyblinders, walkingdead } from '../assets/images'
 
 export const getMovie = () => {
   try {
     return Promise.resolve({
       status: 'success',
       data: [
-        { id: 1, title: 'Kingdom', thumbnail: '', status: '' },
-        { id: 2, name: 'Nadia' },
-        { id: 3, name: 'Handy' },
-        { id: 4, name: 'Fakara' }
+        {
+          id: 1,
+          title: 'Kingdom',
+          thumbnail: kingdom,
+          duration: 7200,
+          type: 'series',
+          genre: ['thriller', 'action', 'drama'],
+          synopsis: 'Lorem ipsum dolor sit amet',
+          release: new Date()
+        },
+        {
+          id: 2,
+          title: 'Breaking Bad',
+          thumbnail: breakingbad,
+          duration: 7200,
+          type: 'series',
+          genre: ['action', 'drama'],
+          synopsis: 'Lorem ipsum dolor sit amet',
+          release: new Date()
+        },
+        {
+          id: 3,
+          title: 'Money Heist',
+          thumbnail: moneyheist,
+          duration: 7200,
+          type: 'series',
+          genre: ['action', 'drama'],
+          synopsis: 'Lorem ipsum dolor sit amet',
+          release: new Date()
+        },
+        {
+          id: 4,
+          title: 'Peaky Blinders',
+          thumbnail: peakyblinders,
+          duration: 7200,
+          type: 'series',
+          genre: ['action', 'drama'],
+          synopsis: 'Lorem ipsum dolor sit amet',
+          release: new Date()
+        },
+        {
+          id: 5,
+          title: 'Walking Dead',
+          thumbnail: walkingdead,
+          duration: 7200,
+          type: 'series',
+          genre: ['action', 'drama'],
+          synopsis: 'Lorem ipsum dolor sit amet',
+          release: new Date()
+        },
       ]
     })
   } catch (e) {
