e58a4d165988b751a7420082ed8abadba0e23e81
faris <putra.faris295@gmail.com>
2021-09-14 00:04:52 +0700

    remove icon

--- src/screens/Movie/views.js
+++ src/screens/Movie/views.js
@@ -4,10 +4,8 @@ import {
   View,
 } from 'react-native';
 import { connect } from 'react-redux';
-import Icon from 'react-native-vector-icons/Ionicons';
 
 import styles from './styles';
-import { Colors } from '../../utils';
 import {
   BaseContainer, AutoComplete, DatePicker, Picker, Upload, Accordion,
 } from '../../components';
@@ -20,7 +18,6 @@ const Movie = () => {
     <>
       <BaseContainer>
         <View style={styles.outerWrapper}>
-          <Icon name="logo-youtube" size={100} color={Colors.red} />
           <View>
             <AutoComplete
               apiUrl="https://api.github.com/users"
@@ -42,25 +39,10 @@ const Movie = () => {
             <Accordion>
               {[...new Array(10)].map(() => (
                 <View>
-
                   <Text>TEST</Text>
                 </View>
               ))}
             </Accordion>
-            {/* {Array.from(Array(1).keys()).map((x) => (
-              <TouchableOpacity
-                key={x.toString()}
-                style={styles.buttonStyle}
-                onPress={() => (isLoading ? {} : dispatch(fetchDataMovie()))}
-              >
-                {!isLoading ?
-                  <Text style={styles.text}>Click here to show Movie data:</Text>
-                  : <Loading size="large" />}
-              </TouchableOpacity>
-
-            ))} */}
-            {/* <ListUsers users={users} /> */}
-            {/* <ListMovie movies={movies} /> */}
           </View>
         </View>
       </BaseContainer>
